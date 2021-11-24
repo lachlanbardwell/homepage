e3a2b4a0d58804b98e67f71e527247d937f35827
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2021-11-24 20:07:49 +1100

    styled components

--- src/client/components/header/header.tsx
+++ src/client/components/header/header.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 
 const HeaderStyles = styled.div`
-  background-color: #282c34;
-  min-height: 100vh;
+  background-color: #071635;
   display: flex;
   flex-direction: column;
+  flex: 0 0 auto;
+  height: 150px;
+  width: 100%;
   align-items: center;
   justify-content: center;
   font-size: calc(10px + 2vmin);
@@ -15,7 +17,7 @@ const HeaderStyles = styled.div`
 export const SuperGHeader: React.FC = () => {
   return (
     <HeaderStyles>
-      <p>Testing Global Styles</p>
+      <header>Header Text</header>
     </HeaderStyles>
   );
 };
--- src/client/styles/global-styles.ts
+++ src/client/styles/global-styles.ts
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+body, #root {
+  height: 100vh
+}
+
 body {
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
@@ -8,6 +12,26 @@ body {
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+#root {
+  display: flex;
+  flex-direction: column;
+}
+
+#root > .App > header, #root > .App > footer {
+    flex: 0 0 auto;
+    height: 200px;
+    background: #071635;
+    width: 100%;
+}
+
+#root > .App > main {
+    flex:  1 1 auto;
+    width: 100%;
+    display: flex;
+    position: relative;
+    justify-content: center;
 }
 
 code {
