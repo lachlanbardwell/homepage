a4986e6ff03d5cc80438fb8de63ee8a807167609
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2022-10-08 22:18:30 +1100

    pic

--- src/client/components/card-link/card-link.styles.ts
+++ src/client/components/card-link/card-link.styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const CardStyles = styled.article`
   margin: auto 5%;
   border-radius: 7px;
+  border: 1px solid #665f16;
   background-color: #fffcda;
   padding: 10px;
   height: 80%;
